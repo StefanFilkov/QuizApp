093efdfbdbbe12a14737b17c6039185cc963da12
StefanFilkov <->
2024-03-02 20:15:47 +0200

    configuration

--- src/main/java/com/example/quizproject/dao/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.quizproject.dao;
-
-import com.example.quizproject.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface QuestionDao extends JpaRepository<Question,Integer> {
-
-}
